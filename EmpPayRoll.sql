e0e2c5f179b99c416be5caf8578339fbddd2b78c
Snehasrg <sghongade642@gmail.com>
2023-01-31 17:23:51 +0530

    table is created

--- EmpPayRoll.sql
+++ EmpPayRoll.sql
@@ -2,3 +2,11 @@ Welcome to Employee Payroll Service Problem.
 
 create database payroll_service;
 use payroll_service;
+
+Create Table employee_payroll
+(
+Id int identity(1,1)primary key not null,
+Name varchar(255),
+Salary int,
+StartDate datetime
+)
